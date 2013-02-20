5d9a943e03ca1649dab994cd551378e69e0131ae
Miguel Peixe <miguel@cardume.art.br>
2013-02-20 16:47:47 -0300

    some start, changing mappress module

---
+++ content-map.php
@@ -0,0 +1,2 @@
+<div class="map-container"><div id="map_<?php echo $post->ID; ?>" class="map"></div></div>
+<script type="text/javascript">mappress({postID: <?php echo $post->ID; ?> });</script>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<?php mappress_map(28); ?>
+
+<?php get_footer(); ?>

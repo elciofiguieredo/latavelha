fe4f87143e6e363d5f89d141a0fdbe81bf6f0a53
Miguel Peixe <miguel@cardume.art.br>
2013-02-21 15:24:22 -0300

    use featured map

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php get_header(); ?>
 
-<div id="map">
+<section id="map">
 	<?php mappress_featured_map(); ?>
+</section>
+	
+<div class="container">
+	<section id="platforms">
+		<h2><?php _e('Platforms', 'latavelha'); ?></h2>
+	</section>
+	<section id="news">
+		<h2><?php _e('News', 'latavelha'); ?></h2>
+	</section>
+	<section id="accidents">
+		<h2><?php _e('Accidents', 'latavelha'); ?></h2>
+	</section>
 </div>
 
 <?php get_footer(); ?>

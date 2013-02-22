2215ec4df610ec1024433bd2b5d61303bacafef4
Miguel Peixe <miguel@cardume.art.br>
2013-02-22 18:02:29 -0300

    rename icons folder and add platform marker

--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@ function latavelha_scripts() {
 	wp_enqueue_style('skeleton', get_stylesheet_directory_uri() . '/css/skeleton.css', array('base'), '1.2');
 	wp_enqueue_style('font-opensans', 'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
 	wp_enqueue_style('font-blackops', 'http://fonts.googleapis.com/css?family=Black+Ops+One');
-	wp_enqueue_style('main', get_stylesheet_directory_uri() . '/css/main.css', array('skeleton', 'font-opensans', 'font-blackops'), '0.0.0.1');
+	wp_enqueue_style('main', get_stylesheet_directory_uri() . '/css/main.css', array('skeleton', 'font-opensans', 'font-blackops'), '0.0.1.1');
 }
 add_action('wp_enqueue_scripts', 'latavelha_scripts');
 
@@ -57,9 +57,23 @@ function latavelha_geocode_metaboxes() {
 add_filter('mappress_markers_query', 'latavelha_markers_query');
 function latavelha_markers_query($query) {
 	if(is_front_page())
-		$query['post_type'] = array('post', 'platform', 'accident', 'oil-well');
+		$query['post_type'] = array('post', 'platform', 'accident');
 
 	return $query;
+}
+
+// lata velha marker icons
+add_filter('mappress_marker_icon', 'latavelha_markers_icon');
+function latavelha_markers_icon($marker) {
+	global $post;
+	if(get_post_type($post->ID) == 'platform') {
+		$marker = array(
+			'url' => get_stylesheet_directory_uri() . '/img/icons/mapa_plataforma_nova.png',
+			'width' => 32,
+			'height' => 47
+		);
+	}
+	return $marker;
 }
 
 ?>
--- index.php
+++ index.php
@@ -5,18 +5,24 @@
 </section>
 	
 <div class="container">
-	<section id="platforms" class="content-section">
-		<h2><?php _e('See platforms', 'latavelha'); ?></h2>
-		<?php query_posts('post_type=platform&posts_per_page=6'); ?>
-		<?php get_template_part('loop', 'platform'); ?>
-		<?php wp_reset_query(); ?>
-	</section>
-	<section id="news" class="content-section">
-		<h2><?php _e('News', 'latavelha'); ?></h2>
-	</section>
-	<section id="accidents" class="content-section">
-		<h2><?php _e('Accidents', 'latavelha'); ?></h2>
-	</section>
+	<div class="twelve columns">
+		<section id="platforms" class="content-section">
+			<h2><?php _e('See platforms', 'latavelha'); ?></h2>
+			<?php query_posts('post_type=platform&posts_per_page=6'); ?>
+			<?php get_template_part('loop', 'platform'); ?>
+			<?php wp_reset_query(); ?>
+		</section>
+	</div>
+	<div class="six columns">
+		<section id="news" class="content-section">
+			<h2><?php _e('News', 'latavelha'); ?></h2>
+		</section>
+	</div>
+	<div class="six columns">
+		<section id="accidents" class="content-section">
+			<h2><?php _e('Accidents', 'latavelha'); ?></h2>
+		</section>
+	</div>
 </div>
 
 <?php get_footer(); ?>

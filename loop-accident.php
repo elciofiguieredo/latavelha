8ca077c252cf46edffcb8b6c4fda0631bd763d6e
Miguel Peixe <miguel@cardume.art.br>
2013-03-21 00:58:18 -0300

    fix accident style bug

--- loop-accident.php
+++ loop-accident.php
@@ -1,18 +1,18 @@
 <?php if(have_posts()) : ?>
 	<div class="loop accident clearfix">
-		<ul>
+		<ul class="masonry-list">
 			<?php while(have_posts()) : the_post(); ?>
-				<li>
+				<li class="masonry-item">
 					<?php get_template_part('card', 'accident'); ?>
 				</li>
 			<?php endwhile; ?>
 		</ul>
 		<script type="text/javascript">
 			jQuery(document).ready(function($) {
-				var $container = $('.loop.accident > ul');
+				var $container = $('.loop.accident .masonry-list');
 				$container.imagesLoaded(function() {
 					$container.isotope({
-						itemSelector: '.loop.accident > ul > li',
+						itemSelector: '.loop.accident .masonry-item',
 						layoutMode: 'masonry'
 					});
 				});

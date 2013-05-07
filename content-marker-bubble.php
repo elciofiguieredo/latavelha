ca44d2bd26fba4463cd060eaab4841a43ab1dea6
Miguel Peixe <miguel@cardume.art.br>
2013-05-07 12:08:37 -0300

    more data to platform bubble

--- content-marker-bubble.php
+++ content-marker-bubble.php
@@ -16,6 +16,16 @@
 	if($date) : ?>
 		<p><strong><?php _e('Date', 'latavelha'); ?></strong>: <?php echo $date; ?></p>
 	<?php endif; ?>
+<?php elseif(get_post_type() == 'platform') :
+	$owner = latavelha_get_platform_owner();
+	$operator = latavelha_get_platform_operator();
+	$age = latavelha_get_platform_age_text();
+	?>
+	<h4><?php the_title(); ?></h4>
+	<p><strong><?php _e('Current status', 'latavelha'); ?></strong>: <span class="status-title <?php echo latavelha_get_platform_status_class(); ?>"><?php echo latavelha_get_platform_status_name(); ?></span></p>
+	<p><strong><?php _e('Owner', 'latavelha'); ?></strong>: <?php echo $owner->name; ?></p>
+	<p><strong><?php _e('Operator', 'latavelha'); ?></strong>: <?php echo $operator->name; ?></p>
+	<p><strong><?php _e('Age', 'latavelha'); ?></strong>: <?php echo $age; ?></p>
 <?php else : ?>
 	<h4><?php the_title(); ?></h4>
 <?php endif; ?>
